f7632897741ff0cd9dc3d52b7241ce8132c5e0f7
normanb98 <107863262+normanb98@users.noreply.github.com>
2022-11-28 12:47:57 +0100

    Update CreateNewCustomerTest.java

--- src/CreateNewCustomerTest.java
+++ src/CreateNewCustomerTest.java
@@ -8,7 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
 
-//New Project
+//New Project Test
 public class CreateNewCustomerTest {
 
     protected WebDriver driver;
